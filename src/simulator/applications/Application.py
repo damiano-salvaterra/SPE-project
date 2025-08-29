42d342bfef87ec2334a46947216a845e50d86cbb
Congiuntivo <simonedecarli19@gmail.com>
2025-08-29 16:26:34 +0200

    Add receive abstract method to Application

--- src/simulator/applications/Application.py
+++ src/simulator/applications/Application.py
@@ -1,16 +1,19 @@
 from abc import ABC, abstractmethod
-#from simulator.entities.physical.devices.nodes import StaticNode
+from simulator.entities.protocols.common.packets import NetPacket
+
 
 class Application(ABC):
     """
     Abstract base class for Node application in the simulator.
     """
+
     def __init__(self):
         super().__init__()
-        
-        
+
     @abstractmethod
     def generate_traffic(self) -> None:
         pass
 
-        
+    @abstractmethod
+    def receive(self, packet: NetPacket, sender_addr: bytes) -> None:
+        pass
