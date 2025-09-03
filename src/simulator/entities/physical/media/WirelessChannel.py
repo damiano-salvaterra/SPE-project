d4363da41d2a23838f8a472dd14a410e4ac7489c
DaMole98 <salvaterra.damiano@gmail.com>
2025-09-03 16:11:08 +0200

    refactored SimplePhyLayer. Redesigned as an explicit state machine and removed the reception session construct. now the phy layer is entirely based on the energy detection, with better represent the physsical phenomenon, is way more efficient and less error prone.

--- src/simulator/entities/physical/media/WirelessChannel.py
+++ src/simulator/entities/physical/media/WirelessChannel.py
@@ -3,22 +3,19 @@ from typing import List, Dict
 
 from simulator.environment.propagation.narrowband import NarrowbandChannelModel
 from simulator.entities.common.Entity import Entity
-from simulator.entities.protocols.phy.common.phy_events import PhyTxEndEvent, PhyTxStartEvent, PhyRxStartEvent, PhyRxEndEvent
-from simulator.entities.protocols.phy.common.ReceptionSession import ReceptionSession
+from simulator.entities.protocols.phy.common.phy_events import PhyRxStartEvent, PhyRxEndEvent
 from simulator.entities.protocols.phy.common.Transmission import Transmission
-from simulator.entities.protocols.common.packets import MACFrame
 from simulator.entities.physical.devices.nodes import StaticNode
 from simulator.engine.common.SimulationContext import SimulationContext
 '''
-This class manages the transmissions and the observation of them to monitor collisions'''
+This class manages the transmissions on the wirelss channel'''
 class WirelessChannel(Entity): # TODO: make this class a singleton
     def __init__(self, propagation_model: NarrowbandChannelModel, nodes: List[StaticNode], context: SimulationContext):
         Entity.__init__(self)
         self.propagation_model = propagation_model
         self.nodes = nodes # list of nodes in the environment
-        #self.listeners: Dict[StaticNode, ReceptionSession] = {} # dict of subscribers tha to observe the channel, subscibed by PhyLayer
         self.context = context
-        self.active_transmissions: Dict[StaticNode, Transmission] = {} # list of currently active transmissions, indexed by the source
+        #self.active_transmissions: Dict[StaticNode, Transmission] = {} # list of currently active transmissions, indexed by the source
         self.tx_counter = 0 # transmission id
 
 
@@ -31,7 +28,7 @@ class WirelessChannel(Entity): # TODO: make this class a singleton
         regardless of the destination, so we need a way to make the tranmission to a node susceptible to any other tranmisssion to any other node.
         This function orchestrate this.
         '''
-        self.active_transmissions[transmission.transmitter] = transmission
+        #self.active_transmissions[transmission.transmitter] = transmission
         transmission.unique_id = self.tx_counter # assign an ID to the transmission
         self.tx_counter += 1 # increment counter
         transmitter_position = transmission.transmitter.position
@@ -49,39 +46,11 @@ class WirelessChannel(Entity): # TODO: make this class a singleton
                 self.context.scheduler.schedule(rx_start_event)
                 self.context.scheduler.schedule(rx_end_event)
 
-            #    # if this receiver has an active session, notify it
-            #    if receiver in self.listeners.keys():
-            #        self.listeners[receiver].notify_tx_start(transmission, propagation_delay) #TODO: i am messing with the future here, maybe i should move the listener to the phy layer and let it manage it from there
-
-        #for listener in self.listeners:
-        #    if listener.capturing_tx is not transmission: #in theory, never happens
-        #        receiver = listener.receiving_node
-        #        propagation_delay = self.propagation_model.propagation_delay(transmitter_position, receiver.position) # notify with propagation delay
-        #        listener.notify_tx_start(transmission, propagation_delay) #notify the observers.
-
 
     def on_PhyTxEndEvent(self, transmission: Transmission):
-            if transmission.transmitter in self.active_transmissions:
-                self.active_transmissions.pop(transmission.transmitter) # remove the transmission from the current transmissions
-        #for session in self.listeners.values(): #TODO: who is notifying the nodes of the end of the transmission  now?
-        #    if session.receiving_node != transmission.transmitter:
-        #        session.notify_tx_end(transmission) #notify the observers
-
-
-
-    #def subscribe_listener(self, session: ReceptionSession):
-    #    '''
-    #    This function subscribes an observer object of the wireless channel.
-    #    This way, we can easily update and notify the observer objects of new interferers/state change of the wireless channel.
-    #    When the transmission ends, we detach the object and compute the reception/collision probability
-    #    TODO: check if it makes more sense to attach PhyLayer objects (of Node) instead of this
-    #    '''
-    #    self.listeners[session.receiving_node] = session
-#
-    #def unsubscribe_listener(self, session: ReceptionSession):
-    #    self.listeners.pop(session.receiving_node)
-
-
+            #if transmission.transmitter in self.active_transmissions:
+            #    self.active_transmissions.pop(transmission.transmitter) # remove the transmission from the current transmissions
+            pass
 
     def get_linear_noise_floor(self) -> float:
         '''
@@ -90,7 +59,6 @@ class WirelessChannel(Entity): # TODO: make this class a singleton
         return self.propagation_model.dBm_to_watts(self.propagation_model.noise_floor_deterministic())
 
         
-
 
     def get_linear_link_budget(self, node1: StaticNode, node2: StaticNode, tx_power_dBm: float) -> float:
         '''
--- src/simulator/entities/protocols/phy/SimplePhyLayer.py
+++ src/simulator/entities/protocols/phy/SimplePhyLayer.py
@@ -1,3 +1,5 @@
+# SPE-project/src/simulator/entities/protocols/phy/SimplePhyLayer.py
+
 from simulator.entities.protocols.common.Layer import Layer
 from simulator.entities.common.Entity import Entity
 from simulator.engine.common.signals import PacketSignal
@@ -5,14 +7,26 @@ from simulator.entities.protocols.phy.common.phy_events import (PhyTxEndEvent, P
 from simulator.entities.protocols.phy.common.Transmission import Transmission
 from simulator.entities.protocols.common.packets import MACFrame, Frame_802154, Ack_802154
 from numpy import log10
+from math import isclose
 from typing import TYPE_CHECKING, Dict
+from enum import Enum, auto
 
 if TYPE_CHECKING:
-    from simulator.entities.protocols.phy.common.ReceptionSession import ReceptionSession
     from simulator.entities.physical.devices.nodes import StaticNode
     from simulator.entities.physical.media.WirelessChannel import WirelessChannel
 
+class RadioState(Enum):
+    """ Defines the operational states of the transceiver. """
+    IDLE = auto()  # Channel is clear, radio is listening.
+    BUSY = auto()  # Receiving energy, but not synchronized to a packet.
+    SYNC = auto()  # Synchronized and actively decoding a specific packet.
+    TX = auto()    # Transmitting a packet.
+
 class SimplePhyLayer(Layer, Entity):
+    """
+    This class implements the physical layer with a state
+    machine model (IDLE, BUSY, SYNC, TX)
+    """
     def __init__(self, host: "StaticNode", transmission_power: float = 0):
         Layer.__init__(self, host=host)
         Entity.__init__(self)
@@ -22,82 +36,153 @@ class SimplePhyLayer(Layer, Entity):
         self.transmission_power = transmission_power
         self.transmission_media = None
 
-        # State management for the receiver
-        self.active_session: "ReceptionSession" = None
-        self.transmitting = False
+        # --- state machine control variables ---
+        self.state: RadioState = RadioState.IDLE
         self.synchronized_tx: "Transmission" = None
         self.reception_power_state: Dict["Transmission", float] = {}
+        self.total_received_power_W: float = 0.0 #currently receiveed power
+        self.min_sinr_db_session: float = float('inf')
 
-        # State for MAC/NET layers
+        # --- State for MAC layer ---
         self._last_seqn = 0
         self._last_successful_rx_rssi_dBm: float = -150.0
 
     def connect_transmission_media(self, transmission_media: "WirelessChannel"):
         self.transmission_media = transmission_media
 
-    def _is_decoded(self, session: "ReceptionSession") -> bool:
-        """
-        Calculates the SINR for the captured transmission and determines if it can be decoded.
-        This method uses the "worst-case SINR" model: the packet is lost if the SINR
-        drops below the capture threshold at any point during the reception.
-        """
-        noise_floor_linear = self.transmission_media.get_linear_noise_floor()
-        capturing_tx_power_linear = self.reception_power_state.get(session.capturing_tx, 0.0)
-        
-        if not session.reception_segments or capturing_tx_power_linear == 0.0:
-            return False
 
-        min_sinr = float('inf')
-        for segment in session.reception_segments:
-            interferers_power = sum(
-                self.reception_power_state.get(tx, 0.0) for tx in segment.interferers
-            )
-            segment_sinr = capturing_tx_power_linear / (noise_floor_linear + interferers_power)
-            if segment_sinr < min_sinr:
-                min_sinr = segment_sinr
 
-        min_sinr_db = 10 * log10(min_sinr) if min_sinr > 0 else -float('inf')
-        return min_sinr_db >= self.capture_threshold_dB
+    # --- State Machine Event Handlers ---
 
     def on_PhyRxStartEvent(self, transmission: Transmission):
-        """
-        Handles the start of a signal's arrival at the receiver.
-        REVISED LOGIC: Implements a "first-come, first-served" model.
-        - If the radio is idle, it attempts to lock onto the new signal.
-        - If the radio is already locked onto a signal, any new arrival is treated as interference.
-        This removes the unrealistic "mid-packet capture" model.
-        """
-        # Sample the channel for the new transmission and store its power.
-        received_power_linear = self.transmission_media.get_linear_link_budget(
+        """ Handles the arrival of a new signal at the receiver """
+        received_power_W = self.transmission_media.get_linear_link_budget(
             node1=self.host, node2=transmission.transmitter, tx_power_dBm=transmission.transmission_power_dBm
         )
-        self.reception_power_state[transmission] = received_power_linear
-
-        # If the radio is already busy receiving a packet, this new signal is just an interferer.
-        if self.active_session:
-            self.active_session.notify_tx_start(transmission=transmission)
+        
+        # If the radio is transmitting, it ignores everything
+        if self.state == RadioState.TX:
             return
 
-        # --- The Radio is currently IDLE ---
-        # Check if the signal is strong enough to be detected and start a reception.
-        received_power_dBm = 10 * log10(received_power_linear * 1000)
-        if received_power_dBm < self.correlator_threshold:
-            # Signal is too weak to be detected. Clean up its state and remain idle.
-            del self.reception_power_state[transmission]
+        # Update the physical state of the channel.
+        self.reception_power_state[transmission] = received_power_W
+        self.total_received_power_W += received_power_W
+
+        if self.state == RadioState.IDLE:
+            # first signal detected: transition from IDLE to a receiving state
+            self._attempt_tx_synchronization(transmission, received_power_W)
+        
+        elif self.state == RadioState.BUSY:
+            # already receiving energy, but the radio is not locked on a transmission: try to sync on this new packet
+            self._attempt_tx_synchronization(transmission, received_power_W)
+
+        elif self.state == RadioState.SYNC:
+            # Decoder is already locked on a packet: this is interference, so update the
+            #minimum SINR of this packet
+            self._update_minimum_SINR()
+
+
+    def on_PhyRxEndEvent(self, transmission: Transmission):
+        """ Handles the end of a signal on the receiver """
+        if transmission not in self.reception_power_state:
+            return # should not happen, but just in case
+
+        ended_power_W = self.reception_power_state.pop(transmission)
+        self.total_received_power_W -= ended_power_W
+        
+        if self.state == RadioState.SYNC and self.synchronized_tx == transmission:
+            # if the the packet we were decoding has just ended, check if is correctly received and clean up
+            self._finalize_rx()
+        
+        # if the channel is now silent, become IDLE, otherwise, stay BUSY
+        if isclose(self.total_received_power_W, 0.0):
+            self.state = RadioState.IDLE
+        elif self.state != RadioState.TX:
+            self.state = RadioState.BUSY
+
+    # --- Transmission Event Handlers ---
+    
+    def on_PhyTxStartEvent(self, transmission: Transmission):
+        """ Handles the start of a transmission from this node's MAC. """
+        self.state = RadioState.TX
+        self.synchronized_tx = None # Cannot be synchronized while transmitting
+        self.transmission_media.on_PhyTxStartEvent(transmission=transmission)
+
+    def on_PhyTxEndEvent(self, transmission: Transmission):
+        """ Handles the end of this node's transmission. """
+        self.transmission_media.on_PhyTxEndEvent(transmission=transmission)
+        self.host.rdc.on_PhyTxEndEvent(packet=transmission.packet)
+        
+        # After transmitting, check the channel state to decide the next state.
+        if isclose(self.total_received_power_W, 0.0):
+            self.state = RadioState.IDLE
+        else:
+            self.state = RadioState.BUSY
+
+    # --- Internal Logic ---
+
+    def _attempt_tx_synchronization(self, transmission: Transmission, power_W: float):
+        """ locks the correlator onto the new transmission, if possible """
+        power_dBm = 10 * log10(power_W * 1000) if power_W > 0 else -float('inf')
+        
+        if power_dBm >= self.correlator_threshold:
+            self.min_sinr_db_session = float('inf') #reset the SINR tracker, just for robustness
+                                                    #because at this point it should be already reset
+            self.state = RadioState.SYNC
+            self.synchronized_tx = transmission
+            self._update_minimum_SINR()
+            self._schedule_sync_check(transmission)
+        else:
+            # the signal is too weak to sync, just go in BUSY state
+            self.state = RadioState.BUSY
+
+
+    def _finalize_rx(self):
+        """ Called when a synchronized packet ends and decides its fate """
+        is_decoded = self.min_sinr_db_session >= self.capture_threshold_dB
+        
+        if is_decoded:
+            received_packet = self.synchronized_tx.packet
+            power_W = self.reception_power_state.get(self.synchronized_tx, 0.0)
+            self._last_successful_rx_rssi_dBm = 10 * log10(power_W * 1000) if power_W > 0 else -150
+            self.receive(payload=received_packet)
+        
+        # The decoder is now free
+        self.synchronized_tx = None
+        self.min_sinr_db_session = float('inf')
+
+    def _update_minimum_SINR(self):
+        """ Computes the instantaneous SINR and updates the minimum for the current locked transmission """
+        if not self.synchronized_tx:
             return
 
-        # The signal is strong enough. Start a new reception session and lock onto it.
-        self._open_session(transmission)
-        self.synchronized_tx = transmission # This is the packet we are now trying to decode.
+        noise_floor_W = self.transmission_media.get_linear_noise_floor()
+        signal_power_W = self.reception_power_state.get(self.synchronized_tx, 0.0)
 
-        # Schedule events to check if the packet is addressed to us after the address fields are received.
+        if isclose(signal_power_W, 0.0):
+            self.min_sinr_db_session = -float('inf')
+            return
+        
+        # Interference is the total received power MINUS the signal power.
+        interference_power_W = self.total_received_power_W - signal_power_W
+        
+        sinr = signal_power_W / (noise_floor_W + interference_power_W)
+        current_sinr_db = 10 * log10(sinr) if sinr > 0 else -float('inf')
+        
+        if current_sinr_db < self.min_sinr_db_session:
+            self.min_sinr_db_session = current_sinr_db
+
+
+    def _schedule_sync_check(self, transmission: "Transmission"):
+        """helper to schedule the moment in which the decoder understands if the packet is of interest or not: if is
+        not of interest, the decoder is desynchronized from the transmission and go in busy state, ready to receive something
+        more interesting"""
         if isinstance(transmission.packet, Ack_802154):
             is_for_me = (transmission.packet.seqn == self._last_seqn)
             detection_time = self.host.context.scheduler.now() + transmission.packet.ack_detection_time
             if not is_for_me:
                 unsync_event = PhyUnsyncEvent(time=detection_time, blame=self, callback=self._unsynchronize)
                 self.host.context.scheduler.schedule(unsync_event)
-        
         elif isinstance(transmission.packet, Frame_802154):
             is_for_me = (transmission.packet.rx_addr == self.host.linkaddr or
                          transmission.packet.rx_addr == Frame_802154.broadcast_linkaddr)
@@ -105,134 +190,51 @@ class SimplePhyLayer(Layer, Entity):
             if not is_for_me:
                 unsync_event = PhyUnsyncEvent(time=detection_time, blame=self, callback=self._unsynchronize)
                 self.host.context.scheduler.schedule(unsync_event)
+    
 
+    
     def _unsynchronize(self):
-        """Called when the radio determines the packet is not for it. It will continue receiving
-        to assess CCA but will not attempt to decode the frame."""
-        self.synchronized_tx = None
-
-    def on_PhyRxEndEvent(self, transmission: Transmission):
-        """
-        Handles the end of a signal's arrival at the receiver.
-        REVISED LOGIC: The radio only makes a decoding decision if the ending transmission
-        was the one it was synchronized to.
-        """
-        # Ignore if there's no active session or if this transmission wasn't being tracked.
-        if not self.active_session or transmission not in self.reception_power_state:
-            self.reception_power_state.pop(transmission, None) # Cleanup just in case
-            return
-
-        is_capturing_tx_ended = (self.active_session.capturing_tx == transmission)
-
-        if is_capturing_tx_ended:
-            # The reception attempt is now complete. We must decide its fate.
-            is_decoded = self._is_decoded(self.active_session)
-            
-            # Retrieve packet info before we clean up the session.
-            received_packet = self.active_session.capturing_tx.packet
-            capturing_tx_power_linear = self.reception_power_state.get(transmission)
-
-            # The reception attempt is over. The radio is now idle.
-            self._close_session()
-
-            # Pass the packet up the stack ONLY if it was successfully decoded and for us.
-            if is_decoded and self.synchronized_tx:
-                rssi_dBm = 10 * log10(capturing_tx_power_linear * 1000)
-                self._last_successful_rx_rssi_dBm = rssi_dBm
-                self.receive(payload=received_packet)
-            # else: The packet was lost to interference/noise or was not for us. Do nothing.
-        else:
-            # An interfering transmission has ended. This improves the SINR for the rest
-            # of our reception. Notify the session and remove the interferer from our state.
-            self.active_session.notify_tx_end(transmission=transmission)
-            self.reception_power_state.pop(transmission, None)
-
-    def _open_session(self, transmission: Transmission):
-        from simulator.entities.protocols.phy.common.ReceptionSession import ReceptionSession
-        
-        # When a new session opens, any other signals already being received are interferers.
-        current_interferers = [
-            tx for tx in self.reception_power_state.keys() if tx is not transmission
-        ]
-
-        self.active_session = ReceptionSession(
-            receiving_node=self.host,
-            capturing_tx=transmission,
-            interferers=current_interferers,
-            start_time=self.host.context.scheduler.now()
-        )
-
-    def _close_session(self):
-        """
-        REVISED: Ends a reception session and fully clears all related receiver state,
-        making the radio truly idle.
-        """
-        if self.active_session:
-            self.active_session = None
+        """ Transitions from SYNC to BUSY because the packet is not for me """
+        if self.state == RadioState.SYNC:
             self.synchronized_tx = None
-            # Crucially, when a reception attempt ends, we no longer track any powers.
-            # A new session will re-sample everything from a clean slate.
-            self.reception_power_state.clear()
-            
-    def on_PhyTxStartEvent(self, transmission: Transmission):
-        self.transmitting = True
-        self.transmission_media.on_PhyTxStartEvent(transmission=transmission)
+            self.min_sinr_db_session = float('inf')
+            self.state = RadioState.BUSY
 
-    def on_PhyTxEndEvent(self, transmission: Transmission):
-        self.transmitting = False
-        self.transmission_media.on_PhyTxEndEvent(transmission=transmission)
-        self.host.rdc.on_PhyTxEndEvent(packet=transmission.packet)
-        
+    # --- Interface for Upper Layers ---
+
     def send(self, payload: MACFrame):
-        signal = PacketSignal(
-            descriptor="PHY Packet Transmission",
-            timestamp=self.host.context.scheduler.now(),
-            event_type="PacketSent",
-            packet=payload
-        )
+        # The 'send' method now triggers the transition to the TX state.
+        signal = PacketSignal("PHY Packet Transmission", self.host.context.scheduler.now(), "PacketSent", payload)
         self._notify_monitors(signal)
 
         if isinstance(payload, Frame_802154):
             self._last_seqn = payload.seqn
 
-        transmission = Transmission(transmitter=self.host, packet=payload, transmission_power_dBm=self.transmission_power)
+        transmission = Transmission(self.host, payload, self.transmission_power)
         
-        start_tx_time = self.host.context.scheduler.now() + 1e-12
-        end_tx_time = start_tx_time + payload.on_air_duration
-        tx_start_event = PhyTxStartEvent(time=start_tx_time, blame=self, callback=self.on_PhyTxStartEvent, transmission=transmission)
-        tx_end_event = PhyTxEndEvent(time=end_tx_time, blame=self, callback=self.on_PhyTxEndEvent, transmission=transmission)
+        start_time = self.host.context.scheduler.now() + 1e-12
+        end_time = start_time + payload.on_air_duration
+        
+        # These events now trigger state transitions within this class.
+        tx_start_event = PhyTxStartEvent(start_time, self, self.on_PhyTxStartEvent, transmission=transmission)
+        tx_end_event = PhyTxEndEvent(end_time, self, self.on_PhyTxEndEvent, transmission=transmission)
 
         self.host.context.scheduler.schedule(tx_start_event)
         self.host.context.scheduler.schedule(tx_end_event)
 
     def cca_802154_Mode1(self) -> bool:
-        if self.is_radio_busy():
-            return True
-        
-        noise_floor = self.transmission_media.get_linear_noise_floor()
-        channel_power = 0.0
-        # Sample all active transmissions on the channel right now.
-        for transmission in self.transmission_media.active_transmissions.values():
-            power_contribute = self.transmission_media.get_linear_link_budget(self.host, transmission.transmitter, transmission.transmission_power_dBm)
-            channel_power += power_contribute
-
-        total_received_power = channel_power + noise_floor
-        total_dBm = 10 * log10(total_received_power * 1000)
-        return total_dBm > self.cca_Threshold_dBm
+        """ CCA is now a simple, efficient check on the total received power. """
+        # No need for a loop. We use the tracked total power.
+        total_power_dBm = 10 * log10(self.total_received_power_W * 1000) if self.total_received_power_W > 0 else -float('inf')
+        return total_power_dBm > self.cca_Threshold_dBm
 
     def is_radio_busy(self) -> bool:
-        # The radio is considered busy if it's transmitting OR in the middle of a reception attempt.
-        return self.transmitting or (self.active_session is not None)
+        """ The radio is busy if it's not in the IDLE state. """
+        return self.state != RadioState.IDLE
 
     def receive(self, payload: MACFrame):
-        signal = PacketSignal(
-            descriptor="PHY Packet Reception",
-            timestamp=self.host.context.scheduler.now(),
-            event_type="PacketReceived",
-            packet=payload
-        )
+        signal = PacketSignal("PHY Packet Reception", self.host.context.scheduler.now(), "PacketReceived", payload)
         self._notify_monitors(signal)
-
         self.host.rdc.receive(payload=payload)
 
     def get_last_rssi(self) -> float:
