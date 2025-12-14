e9092a29deb936a7922cd2f1fc556fa4c00c018c
Congiuntivo <simonedecarli19@gmail.com>
2025-12-14 16:45:40 +0100

    Fix interarrival time calculation on IT monitor

--- src/experiments/experiment_monitors/InterarrivalTimeMonitor.py
+++ src/experiments/experiment_monitors/InterarrivalTimeMonitor.py
@@ -1,9 +1,9 @@
-import pandas as pd
 from typing import TYPE_CHECKING
 
 from simulator.engine.common.Monitor import Monitor
 from simulator.entities.applications.common.app_signals import (
     AppProcessStartSignal,
+    AppSendFailSignal,
     AppSendSignal,
 )
 
@@ -20,35 +20,40 @@ class InterarrivalTimeMonitor(Monitor):
     def __init__(self, monitor_name: str = "IT", verbose=True):
         super().__init__(monitor_name=monitor_name, verbose=verbose)
 
-        self.arrival_times_map = {} #key: emitting host linkaddr, value: list of arrival times
-
+        self.arrival_times_map = (
+            {}
+        )  # key: emitting host linkaddr, value: list of arrival times
 
     def update(self, entity: "Entity", signal: "EntitySignal"):
 
         if not hasattr(signal, "get_log_data"):
             return
 
-        if not isinstance(signal, (AppSendSignal, AppProcessStartSignal)):
+        if not isinstance(
+            signal, (AppSendSignal, AppProcessStartSignal, AppSendFailSignal)
+        ):
+
             return  # consider only send signals
-        
-        
-        
+
         host_addr = entity.host._linkaddr.hex()
-        
+
         if isinstance(signal, AppProcessStartSignal):
-            #New app started: put the host in the dict and set reference
+            # New app started: put the host in the dict and set reference
             if host_addr not in self.arrival_times_map.keys():
                 self.arrival_times_map[host_addr] = []
                 self.arrival_times_map[host_addr].append(signal.timestamp)
 
             if self.verbose:
-                    print(
-                        f"[IT_MONITOR] [{signal.timestamp:.6f}s] [{entity.host.id}] App Process Started"
-                    )
+                print(
+                    f"[IT_MONITOR] [{signal.timestamp:.6f}s] [{entity.host.id}] App Process Started"
+                )
 
-        elif isinstance(signal, AppSendSignal):
+        elif isinstance(signal, (AppSendFailSignal, AppSendSignal)):
+
             if host_addr not in self.arrival_times_map.keys():
-                self.arrival_times_map[host_addr] = [] #create the key if it missed the process start, there is no reference anyways since hte diffs are computed at the end
+                self.arrival_times_map[host_addr] = (
+                    []
+                )  # create the key if it missed the process start, there is no reference anyways since hte diffs are computed at the end
 
             self.arrival_times_map[host_addr].append(signal.timestamp)
 
@@ -58,24 +63,21 @@ class InterarrivalTimeMonitor(Monitor):
                                                                                                                                  - self.arrival_times_map[host_addr][-2]):.3f}s"
                 )
 
-
-
-        
     def save_to_csv(self, base_path: str):
-        #convert the dict into a list (of length 1) of dict with key: host, value: mean interarrival time
-        log_row = {} #row of the csv: one column for each host. Only one row with the average IT
-        for host, at_vec in self.arrival_times_map.items(): #get inter-arrival times
+        # convert the dict into a list (of length 1) of dict with key: host, value: mean interarrival time
+        log_row = (
+            {}
+        )  # row of the csv: one column for each host. Only one row with the average IT
+        for host, at_vec in self.arrival_times_map.items():  # get inter-arrival times
             diffs = []
-            for i in range(0, len(at_vec)-1):
-                diffs.append(at_vec[i+1]-at_vec[i]) #inter-arrival times vector
+            for i in range(0, len(at_vec) - 1):
+                diffs.append(at_vec[i + 1] - at_vec[i])  # inter-arrival times vector
             if len(diffs) > 0:
-                mean_it = sum(diffs)/len(diffs) #compute mean
+                mean_it = sum(diffs) / len(diffs)  # compute mean
             else:
                 mean_it = 0.0
             log_row[host] = mean_it
-        #create list of dict as requested by Monitor base class
+        # create list of dict as requested by Monitor base class
         self.log = [log_row]
 
         super().save_to_csv(base_path)
-
-                
